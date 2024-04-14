8b56c8941769db06dc2ec6d1d33ce8c72baf7540
ZHAO, Haoyu <hzhaoar@umich.edu>
2024-04-14 16:27:19 -0400

    mapping dict added

--- zhy_module.py
+++ zhy_module.py
@@ -1,6 +1,7 @@
 # %%
 import numpy as np
 import pandas as pd
+
 # %%
 df = pd.read_csv('U.S._Chronic_Disease_Indicators.csv')
 
@@ -12,36 +13,16 @@ us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL',
              'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 
              'VA', 'WA', 'WV', 'WI', 'WY']
 
-# Filter the DataFrame to include only rows with LocationAbbr in the list of US state abbreviations
 df_us_states = df[df['LocationAbbr'].isin(us_states)]
-
 
 # %%
 df_filtered = df[(df['DataValueAlt'] != df['DataValue']) & df['DataValueAlt'].notna() & df['DataValue'].notna()]
 df_filtered[['DataValueAlt', 'DataValue']]
 
-
 # %%
-mappings = {
-    'Topic': 'TopicID',
-    'Question': 'QuestionID',
-    'DataValueType': 'DataValueTypeID',
-    'StratificationCategory1': 'StratificationCategoryID1',
-    'Stratification1': 'StratificationID1',
-    'LocationDesc': 'LocationAbbr'
-}
-
-# Iterate over each mapping and save to a new CSV file
-for column, id_column in mappings.items():
-    # Select the columns
-    mapping_df = df[[column, id_column]]
-    
-    # Drop duplicate rows to ensure one-to-one mapping
-    mapping_df = mapping_df.drop_duplicates()
-    
-    # Save the mapping relation to a new CSV file
-    mapping_df.to_csv(f'{column.lower()}_mapping.csv', index=False)
-    
-    # Print a confirmation message
-    print(f"Mapping relation for '{column}' saved to '{column.lower()}_mapping.csv'")
-# %%
+topic_mapping = df[['Topic', 'TopicID']].drop_duplicates().set_index('Topic').to_dict()['TopicID']
+question_mapping = df[['Question', 'QuestionID']].drop_duplicates().set_index('Question').to_dict()['QuestionID']
+datavalue_type_mapping = df[['DataValueType', 'DataValueTypeID']].drop_duplicates().set_index('DataValueType').to_dict()['DataValueTypeID']
+stratification_category_mapping = df[['StratificationCategory1', 'StratificationCategoryID1']].drop_duplicates().set_index('StratificationCategory1').to_dict()['StratificationCategoryID1']
+stratification_mapping = df[['Stratification1', 'StratificationID1']].drop_duplicates().set_index('Stratification1').to_dict()['StratificationID1']
+location_mapping = df[['LocationDesc', 'LocationAbbr']].drop_duplicates().set_index('LocationDesc').to_dict()['LocationAbbr']
