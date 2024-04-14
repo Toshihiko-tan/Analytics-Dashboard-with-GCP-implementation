41b68e1a1e3c14c233f0ea8c7897583d1e778ffd
Samuel Tan <tansam@umich.edu>
2024-04-14 16:11:19 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
 # This is the main module that imports the two modules and runs the functions
-from data_cleaning_module import *
-from visualization_and_prediction_module import *
+
+import data_cleaning_module
+import visualization_and_prediction_module
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+import numpy as np
+from data_cleaning_module import df
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Hello Dash'),
+
+    html.Div('''
+        Dash: A web application framework for your data. 
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        # figure=fig
+    ),
+
+    html.Div('''
+        Dash: Another example for chart
+    '''),
+
+    dcc.Graph(
+        id='example-graph2',
+        # figure=fig2
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8080)
---
+++ samuel_tan_module.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+df = pd.read_csv("U.S._Chronic_Disease_Indicators.csv")
+
+print(df.describe())
